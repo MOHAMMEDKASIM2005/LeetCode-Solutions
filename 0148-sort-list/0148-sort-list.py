27b281785130ff5c4d4eab5c07e21718f52e5233
A.Mohammed Kasim <kafoora514@gmail.com>
2026-08-27 11:54:54 +0530

    Time: 181 ms (39.25%), Space: 40.9 MB (15.86%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,32 @@
+class Solution:
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+        
+        # Find middle using slow/fast pointers
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        mid = slow.next
+        slow.next = None  # split into two halves
+        
+        left = self.sortList(head)
+        right = self.sortList(mid)
+        
+        return self.merge(left, right)
+    
+    def merge(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        tail.next = l1 or l2
+        return dummy.next
