7c69c97f73162dbfe1a20d2e2900ec6d1f5f7050
A.Mohammed Kasim <kafoora514@gmail.com>
2026-08-28 18:33:55 +0530

    Time: 35 ms (88.9%), Space: 33.5 MB (87.03%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,34 @@
+class Solution:
+    def partition(self, s):
+        result = []
+        path = []
+
+        def isPalindrome(string):
+            return string == string[::-1]
+
+        def backtrack(start):
+
+            # Base case
+            if start == len(s):
+                result.append(path[:])
+                return
+
+            # Try every possible substring
+            for end in range(start + 1, len(s) + 1):
+
+                substring = s[start:end]
+
+                # Only continue if palindrome
+                if isPalindrome(substring):
+                    path.append(substring)
+                    backtrack(end)
+                    path.pop()
+
+        backtrack(0)
+
+        return result
+
+
+sol = Solution()
+s = "aab"
+print(sol.partition(s))
