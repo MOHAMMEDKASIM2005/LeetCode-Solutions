52e1ac4be47d0154ddd3f6a5295fc75a2ae9409d
A.Mohammed Kasim <kafoora514@gmail.com>
2026-09-03 22:22:02 +0530

    Time: 307 ms (49.16%), Space: 60 MB (80.47%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfWeakCharacters(self, properties):
+        properties.sort(key=lambda x: (x[0], -x[1]))
+
+        max_defense = 0
+        count = 0
+
+        for attack, defense in reversed(properties):
+            if defense < max_defense:
+                count += 1
+
+            max_defense = max(max_defense, defense)
+
+        return count
